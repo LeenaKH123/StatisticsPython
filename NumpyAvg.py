9c216bf49a94a4c2076a19e546d21a1303e881fd
Leena <engleena@yahoo.com>
2022-04-04 11:19:27 +0300

    the average age of the authors, and CSV created from scratch

--- NumpyAvg.py
+++ NumpyAvg.py
@@ -3,12 +3,14 @@ import numpy as np
 import pandas as pd
 
 # Read author data
-greatest_books = pd.read_csv("hundredbooks.csv")
+greatest_books = pd.read_csv("StatisticsPython/hundredbooks.csv")
+print(greatest_books)
 
 # Set author ages to a NumPy array
-author_ages = greatest_books['Ages']
+author_ages = greatest_books['ages']
+print(author_ages)
 
-# Use numpy to calculate the average age of the top 100 authors
-average_age = 0
+# # Use numpy to calculate the average age of the top 100 authors
+average_age = np.average(author_ages)
 
-print("The average age of the 100 greatest authors, according to a survey by Le Monde, is: " + str(average_age))
+print("The average age of the authors, is: " + str(average_age))
